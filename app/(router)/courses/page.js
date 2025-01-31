ec6323bb9a053395db8a05fec706cace6a4cd440
UnicornDad8 <unicorndad8@gmail.com>
2025-01-31 13:28:35 -0300

    Grid position of banner and right section on /courses route

--- app/(router)/courses/page.js
+++ app/(router)/courses/page.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Courses = () => {
   return (
-    <div>Courses</div>
+    <div className="grid grid-cols-1 md:grid-cols-3">
+      {/* Left Container */}
+      <div className="col-span-2">
+        Banner
+      </div>
+      {/* Right Container */}
+      <div>
+        Right Section
+      </div>
+    </div>
   )
 }
 
