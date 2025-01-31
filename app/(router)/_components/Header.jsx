53e512cb66df47b9807979240374dd93747f46c2
UnicornDad8 <unicorndad8@gmail.com>
2025-01-31 12:50:57 -0300

    Add flex 1 to search bar

--- app/(router)/_components/Header.jsx
+++ app/(router)/_components/Header.jsx
@@ -4,13 +4,13 @@ import MobileNav from './MobileNav'
 
 const Header = () => {
   return (
-    <div className="p-4 bg-white flex justify-between">
-      <div className="md:hidden">
+    <div className="p-4 bg-white flex gap-4 justify-between">
+      <div className="sm:hidden">
         <MobileNav />
       </div>
-      <div className="flex gap-2 border rounded-md p-2">
+      <div className="flex flex-1 gap-2 border rounded-md p-2">
         <Search className="w-5 h-5" />
-        <input type="text" placeholder="Search" className="outline-none" />
+        <input type="text" placeholder="Search" className="outline-none w-full" />
       </div>
       <div className="flex items-center gap-4">
         <BellDot className="text-gray-500" />
